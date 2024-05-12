f608741746a5a51078092d3839d6e77edc2927d3
Kumuditha2001 <kumudithakumarasinghe2001@gmail.com>
2024-05-12 20:30:43 +0530

    Initial commit

---
+++ app/src/main/java/com/example/todo/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.todo
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.todo.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.addButton.setOnClickListener {
+            val intent = Intent(this,AddNoteActivity::class.java)
+        }
+    }
+}
